7c005584de0944b74941b3559a510f85a4ba3e89
Mohamed Elkholy <mg.elkholy90@gmail.com>
2020-09-16 04:36:17 +0200

    edit table name

--- app/Settings.php
+++ app/Settings.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Settings extends Model
 {
-    protected $table = 'Settings';
+    protected $table = 'settings';
     protected $fillable = ['store_name','store_logo','status','close_message','logo','icon','invoice_message','phone_number','address','commercial_register'];
 }
